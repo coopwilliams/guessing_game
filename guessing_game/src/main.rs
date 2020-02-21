6fb20d9531b473b2cc4521fecbe023b91023a2d1
Coop Williams <ken.c.williams.iii@gmail.com>
2020-02-21 02:04:41 -0500

    quit on correct

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -9,23 +9,28 @@ fn main() {
 
     println!("Secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    println!("thanks.");
+        println!("thanks.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You got it!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You got it!");
+                break;
+            }
+        }
     }
 }
