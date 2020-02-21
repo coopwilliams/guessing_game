f27779e9003c65ff0d61aef1c48f605bb2b1cef8
Coop Williams <ken.c.williams.iii@gmail.com>
2020-02-21 02:00:50 -0500

    check guesses

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,31 @@
-use std:io;
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
-fn main(){
+fn main() {
     println!("Guess the number!");
 
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("Secret number is: {}", secret_number);
+
     println!("Please input your guess.");
+
+    println!("thanks.");
 
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You got it!"),
+    }
 }
